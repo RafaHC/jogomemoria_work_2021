8303a5429064afc385e14c648f9074070f218990
Rafael Henrique <rafaelcandido.gft@sulamerica.com.br>
2021-06-12 13:27:16 -0300

    Subindo Jogo da memoria

--- main.js
+++ main.js
@@ -1,67 +1,129 @@
-const cardBoard = document.querySelector("#cardboard");
-const imgs = [
-  "vue.svg",
-  "angular.svg",
-  "react.svg",
-  "ember.svg",
-  "backbone.svg",
-  "aurelia.svg"
+let jogadores = [
+    { numero: 1, nome: '', jogar: true, acertos: 0, erros: 0 },
+    { numero: 2, nome: '', jogar: false, acertos: 0, erros: 0 }
 ];
 
-let cardHTML = "";
 
-imgs.forEach(img => {
-  cardHTML += `<div class="memory-card" data-card="${img}">
-    <img class="front-face" src="img/${img}"/>
-    <img class="back-face" src="img/js-badge.svg">
-  </div>`;
-});
 
-cardBoard.innerHTML = cardHTML + cardHTML;
 
-/** Fim da Renderização HTML */
+
+const cardBoard = document.querySelector("#cardboard");
+
+const imgs = montarJogo();
+
+cardBoard.innerHTML = desenharJogo(imgs);
 
 const cards = document.querySelectorAll(".memory-card");
 let firstCard, secondCard;
 let lockCards = false;
 
+cards.forEach(card => card.addEventListener("click", flipCard));
+
 function flipCard() {
-  if (lockCards) return false;
-  this.classList.add("flip");
+    if (lockCards) return false;
+    this.classList.add("flip");
 
-  if (!firstCard) {
-    firstCard = this;
-    return false;
-  }
+    if (!firstCard) {
+        firstCard = this;
+        return false;
+    }
 
-  secondCard = this;
+    secondCard = this;
 
-  checkForMatch();
+    checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
 
-  !isMatch ? unFlipCards() : resetCards(isMatch);
+    !isMatch ? unFlipCards() : resetCards(isMatch);
 }
 
 function unFlipCards() {
-  lockCards = true;
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
+    lockCards = true;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        contabilizarErros();
+        console.log(jogadores);
 
-    resetCards();
-  }, 1000);
+        resetCards();
+    }, 1000);
 }
 
 function resetCards(isMatch = false) {
-  if (isMatch) {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-  }
+    if (isMatch) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+        contabilizarAcertos();
+        console.log(jogadores);
 
-  [firstCard, secondCard, lockCards] = [null, null, false];
+    }
+
+    [firstCard, secondCard, lockCards] = [null, null, false];
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+function contabilizarAcertos() {
+    jogadores.forEach(jogador => {
+        if (jogador.jogar) jogador.acertos += 1;
+    });
+
+    document.getElementById('resultado').innerHTML = `<br><h2>Resultado do Jogo: ${jogadores[0].nome} - ${jogadores[0].acertos} X ${jogadores[1].nome} - ${jogadores[1].acertos}  </h2>`;
+}
+
+function contabilizarErros() {
+    jogadores.forEach(jogador => {
+        if (jogador.jogar) jogador.erros += 1;
+        jogador.jogar = !jogador.jogar;
+
+    })
+}
+
+
+function embaralhar(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
+
+function definirNomeJogadores() {
+    jogadores.forEach((jog) => {
+        jog.nome = prompt(`Digite o nome do jogador ${jog.numero}`);
+    });
+}
+
+function montarJogo() {
+
+    definirNomeJogadores();
+    console.log(jogadores);
+    let imgs = [
+        'foto1.jpeg',
+        'foto2.jpeg',
+        'foto3.jpeg',
+        'foto4.jpeg',
+        'foto5.jpeg',
+        'foto6.jpeg'
+    ];
+
+    /*  "vue.svg",
+      "angular.svg",
+      "react.svg",
+      "ember.svg",
+      "backbone.svg",
+      "aurelia.svg"*/
+
+    imgs = embaralhar(imgs);
+    return imgs;
+}
+
+
+function desenharJogo(imgs) {
+    let cardHTML = "";
+
+    imgs.forEach(img => {
+        cardHTML += `<div class="memory-card" data-card="${img}">
+      <img class="front-face" src="img/${img}"/>
+      <img class="back-face" src="img/cosmeticos.jpg">
+    </div>`;
+    });
+    return cardHTML + cardHTML + `<div id='resultado'></div>`;
+}
